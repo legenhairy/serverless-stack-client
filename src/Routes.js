3adf1e08938fc8707b67d8d620bd8232661a9586
Harry Zhang <harryzhang100@gmail.com>
2019-10-14 21:32:26 -0700

    setting up the react app

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+
+export default function Routes() {
+  return (
+  	<Switch>
+  	  <Route path='/' exact component={Home} />	
+  	  <Route component={NotFound} />	
+  	</Switch>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "note-file-upload1"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://7aew5k9cu3.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_6riLgZLAz",
+    APP_CLIENT_ID: "3mp5l54sg9tkkhil12scjbmot2",
+    IDENTITY_POOL_ID: "us-east-2:79de8c9e-21dd-47d7-b14c-85afe5206fb9"
+  }
+};
